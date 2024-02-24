6b8dd520866ab4e5146ff141a3523ced07c019ee
Abdool-Zaid <aztoorabally7447@gmail.com>
2024-02-24 16:11:54 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,54 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+export default {
+  data() {
+    return {
+      range: 20,
+      ammount: 1,
+      total: 1,
+      res: []
+    };
+  },
+  methods: {
+    gen_ran(inp) {
+      return Math.max(1, Math.round(Math.random() * inp));
+    },
+    run() {
+      this.res = [];
+      this.total = 0;
+      for (let index = 0; index < this.ammount; index++) {
+        const diceValue = this.gen_ran(this.range);
+        this.total += diceValue;
+        this.res.push(diceValue);
+      }
+    }
+  }
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div>
+    <div class="input">
+      <label for="dice_count">Amount of dice : {{ ammount }}</label>
+      <input type="range" name="dice_count" id="dice_count" v-model="ammount" min="1" max="66">
+      <label for="range">Range 1- {{ range }}</label>
+      <input type="range" name="range" id="range" v-model="range" min="1" max="100">
+      <button @click="run">Roll</button>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="output">
+      <div v-for="(dice, index) in res" :key="index" class="dice">Dice {{ index + 1 }}: {{ dice }}</div>
+      <h1>Total: {{ total }}</h1>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.input {
+  display: flex;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.output {
+  display: flex;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.dice {
+  background-color: green;
 }
 </style>
